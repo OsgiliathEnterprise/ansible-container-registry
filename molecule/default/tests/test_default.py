e0046dcad0d5cbbaa9da1cb4dd630c2dc8c51fe9
Charlie Mordant <cmordant1@gmail.com>
2025-11-10 20:15:31 +0100

    latest ansible

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,9 +1,8 @@
 import os
-
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('client.osgiliath.test')
 
 
 def test_is_docker_installed(host):
